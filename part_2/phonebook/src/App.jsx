cab8fe40c3883b6b099456ab23b4f8af2527a5ea
wasim <w.akram.1912@protonmail.com>
2022-11-13 15:25:07 +0530

    data fetching & useEffect

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import { useState } from 'react';
 import { AddContact } from './components/AddContact';
 import { ContactList } from './components/ContactList';
 import { Search } from './components/Search';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then((res) => setPersons(res.data));
+  }, []);
 
   function handleSubmit(e) {
     e.preventDefault();
